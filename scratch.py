c3de5c4ec50f5188e3fbfc776a7c000b68c6a4d8
Steven Comer <comeste10@gmail.com>
2024-10-20 21:42:15 -0500

    new test for how to revert

--- scratch.py
+++ scratch.py
@@ -1,74 +1,13 @@
-# garbage insertion to test update
-# second minor edit
-# third edit, this time from new 'iss2' branch
-# fifth edit, this time from new 'sfc_vs_win' branch
+def required_function():
+    print("required")
+    i=1
+    i+=2
+    print(i)
+    return
 
-# 10/20/2024: step one, merging/pulling this branch back into main
-
-
-
-# 10/20/2024 main currently checked out, trying to get main ahead so a merge is required
-
-import cmd
-import turtle
-
-class TurtleShell(cmd.Cmd):
-    intro = 'Welcome to the Turtle Shell. Type help or ? to list commands.\n'
-    prompt = 'turtle> '
-
-    def __init__(self):
-        super().__init__()
-        self.turtle = turtle.Turtle()
-        self.screen = turtle.Screen()
-
-    def do_forward(self, arg):
-        'Move the turtle forward by the specified distance.'
-        try:
-            distance = int(arg)
-            self.turtle.forward(distance)
-        except ValueError:
-            print('Invalid distance.')
-
-    def do_backward(self, arg):
-        'Move the turtle backward by the specified distance.'
-        try:
-            distance = int(arg)
-            self.turtle.backward(distance)
-        except ValueError:
-            print('Invalid distance.')
-
-    def do_right(self, arg):
-        'Turn the turtle right by the specified angle.'
-        try:
-            angle = int(arg)
-            self.turtle.right(angle)
-        except ValueError:
-            print('Invalid angle.')
-
-    def do_left(self, arg):
-        'Turn the turtle left by the specified angle.'
-        try:
-            angle = int(arg)
-            self.turtle.left(angle)
-        except ValueError:
-            print('Invalid angle.')
-
-    def do_penup(self, arg):
-        'Lift the turtle\'s pen up.'
-        self.turtle.penup()
-
-    def do_pendown(self, arg):
-        'Put the turtle\'s pen down.'
-        self.turtle.pendown()
-
-    def do_color(self, arg):
-        'Set the turtle\'s pen color.'
-        self.turtle.color(arg)
-
-    def do_exit(self, arg):
-        'Exit the turtle shell.'
-        print('Exiting...')
-        return True
+def main():
+    required_function()
+    return
 
 if __name__ == '__main__':
-    TurtleShell().cmdloop()
+    main()
